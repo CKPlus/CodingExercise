841860d75ff815cd7c8f2c3c47fb3bbf105bd455
EricYu <chunkaiyu@gogolook.com>
2018-05-05 16:54:26 +0800

    Add data source for app

---
+++ data_source.py
@@ -0,0 +1,13 @@
+from flask import current_app
+
+
+class DataSource(object):
+    data = {}
+
+    def __init__(self, app=None):
+        self.app = app
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        pass
