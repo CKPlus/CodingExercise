eff02abf8a9671d42d5ccfa08f74f9d36609cb75
EricYu <chunkaiyu@gogolook.com>
2018-05-05 15:09:05 +0800

    Enable after_request for attach reponse headers

--- web_api/__init__.py
+++ web_api/__init__.py
@@ -1,13 +1,28 @@
-from flask import Flask
+from flask import Flask, request, session
+
+# Views blueprint
+from web_api.topics.views import TOPICS_BP
 
 
 def create_app():
     flask_app = Flask(__name__)
-
+    flask_app.secret_key = '9bc853e5-0e93-46aa-836a-1d1799f999c2'
     return flask_app
 
 
 app = create_app()
 
-from web_api.topics.views import TOPICS_BP
 app.register_blueprint(TOPICS_BP, url_prefix='/topics')
+
+
+@app.after_request
+def after_request(response):
+    '''
+        1. setting response header
+    '''
+    if response.headers.get("Content-Length", "0") != "0" \
+            and not 'admin' in request.path:
+        response.headers["Content-Type"] = "application/json"
+
+    session.clear()
+    return response
